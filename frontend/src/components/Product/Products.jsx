076b24da3d5f6ada64fedad1a56e646ae0416dc5
bharat <bhartbhammar3336@gmail.com>
2024-01-05 13:48:03 +0530

    UI UPdate: Responsiveness

--- frontend/src/components/Product/Products.jsx
+++ frontend/src/components/Product/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { IoIosArrowForward } from "react-icons/io";
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,7 +7,10 @@ import { clearErrors, getProducts } from '../../actions/productActions';
 import ModernProductCard from '../Utils/ModernProductCard';
 import MetaData from '../layout/MetaData';
 import './Products.css';
-
+import { Rating, Slider } from '@mui/material';
+const categories = [
+    'Mobiles', 'Laptops', 'Watches', 'Cameras', 'TVS', 'Mens', 'Womens'
+]
 const Products = () => {
     const location = useLocation()
     const queryParams = new URLSearchParams(location.search)
@@ -20,8 +23,9 @@ const Products = () => {
             toast.error(error);
             dispatch(clearErrors);
         }
-        dispatch(getProducts(keyword, 1, [0, 10000]));
+        dispatch(getProducts(keyword, 1, [0, 100000]));
     }, [dispatch, error, keyword]);
+
 
     return (
         <>
@@ -29,24 +33,32 @@ const Products = () => {
             <h2 className="productHeading">Products</h2>
             <div className="products-container">
                 <div className="products-sidebar">
-                    {/* <div className='filter-option'>
-                        <span>Price</span>
-                        <span><IoIosArrowForward /></span>
-                    </div>
 
-                    <div className='filter-option'>
-                        <span>Category</span>
-                        <span><IoIosArrowForward /></span>
-                    </div>
 
-                    <div className='filter-option'>
-                        <span>Ratings</span>
-                        <span><IoIosArrowForward /></span>
-                    </div> */}
-
-                    <FilterOption title={"Price"} />
-                    <FilterOption title={"Category"} />
-                    <FilterOption title={"Ratings"} />
+                    <FilterOption title={"Price"} >
+                        <div className='filter-container'>
+                            <Slider
+                                track="normal"
+                                getAriaValueText={(value) => `${value} Rs`}
+                                defaultValue={[1, 100000]}
+                                max={100000}
+                                min={1}
+                                size='small'
+                                valueLabelDisplay='on'
+                            />
+                        </div>
+                    </FilterOption>
+                    <FilterOption title={"Category"} >
+                        <div className='filter-container'>
+                            {categories.map(cat => <li cl>{cat}</li>)}
+                        </div>
+                    </FilterOption>
+                    <FilterOption title={"Ratings"} >
+                        <div className='filter-container'>
+                            <h3>Rating Above:</h3>
+                            <Rating precision={.5} />
+                        </div>
+                    </FilterOption>
                 </div>
 
                 <main className="products-main">
@@ -60,10 +72,15 @@ const Products = () => {
     );
 };
 
-export const FilterOption = ({ title, data }) => {
-    return (<div className='filter-option'>
+export const FilterOption = ({ title, children }) => {
+    const [isOpen, setIsOpen] = useState(false)
+    return (<><div className='filter-option' onClick={e => setIsOpen(state => !state)}>
         <span>{title}</span>
         <span><IoIosArrowForward /></span>
-    </div>)
+    </div >
+        {isOpen && children
+        }
+    </>
+    )
 }
 export default Products;
